1889d0a93a777bc02394f6a8517b20d34bc6994f
Jessie <jessiereffet@hotmail.com>
2017-07-12 14:46:17 +0200

    CSS grid is on

--- src/App.js
+++ src/App.js
@@ -41,31 +41,32 @@ class App extends Component {
     const { shortcuts, tags } = this.data;
     const { text } = this.state;
     return (
-      <div className="app">
-        <Search filterTable={this._filterTable}/>
-        <main className="main-grid">
-          <div className="sidebar-grid">
-            <AddShortcutForm addShortcut={this._addShortcut} />
-            <section className="section-tags">
-            {tags
-              .map(t =>
-                <Checkbox
-                  key={t}
-                  tag={t}
-                  textInput={text}
-                  selectTag={this._selectTag}
-                  filterTable={this._filterTable}
-                />
-              )
-
-          }
-            </section>
-          </div>
+      <div className="app grid">
+        <header>HEADER</header>
+        <main className="">
+          <Search filterTable={this._filterTable}/>
           <Table
             shortcuts={shortcuts} textInput={text}
             selectedTags={this.selectedTags}
           />
         </main>
+        <div className="sidebar-grid">
+          <AddShortcutForm addShortcut={this._addShortcut} />
+          <section className="section-tags">
+          {tags
+            .map(t =>
+              <Checkbox
+                key={t}
+                tag={t}
+                textInput={text}
+                selectTag={this._selectTag}
+                filterTable={this._filterTable}
+              />
+            )
+          }
+            </section>
+          </div>
+          <footer>FOOTER</footer>
       </div>
     );
   }
